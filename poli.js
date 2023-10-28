589c2dd51a70686029e5c40b2e3a355bd6fa9795
Ahilan Karuppusami <ahilanks101@gmail.com>
2023-10-28 11:11:36 -0700

    adding initial files; initiating file transfer

---
+++ poli.js
@@ -0,0 +1,9 @@
+// JavaScript code for handling user interactions and sending requests to the server
+document.getElementById('submit-button').addEventListener('click', function() {
+    // Get user preferences and send them to the server
+    const preferences = {
+        // Collect user preferences here
+    };
+    // Send preferences to the server using AJAX or fetch
+    // Process the response to display stock suggestions
+});
---
+++ poli.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name)
+
+@app.route('/get_stock_suggestions', methods=['POST'])
+def get_stock_suggestions():
+    # Retrieve user preferences from the request
+    user_preferences = request.json  # Use JSON format for the request
+
+    # Perform data processing, fetch data from OpenSecrets, and generate stock suggestions
+
+    # Return stock suggestions as JSON
+    suggestions = {
+        'suggestions': [
+            # List of stock suggestions
+        ]
+    }
+    return jsonify(suggestions)
+
+if __name__ == '__main':
+    app.run(debug=True)
