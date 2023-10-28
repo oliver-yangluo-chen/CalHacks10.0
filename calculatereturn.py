4ea07370084674c22303d5fa3a341ac8d67f21c8
Oliver Chen <oliverchen5285@gmail.com>
2023-10-28 15:51:47 -0700

    new logo

--- calculatereturn.py
+++ calculatereturn.py
@@ -1,11 +1,29 @@
 from getstocks import *
 from getpolis import *
 
+
+
 def getreturn(name: str):
     #get all rows with name
     rows = getnamerows(name)
-    trades = dict()
+    rows.sort_values(by=['TransactionDate'], ascending = True)
+    #print(rows)
+    for index, row in rows.iterrows(): print(rows.at[index, 'TransactionDate'])
+    return
+    shares = dict() #ticker: number of shares
+    spent = 0.0 #sum of purchases
+    earned = 0.0 #sum of sells
     for index, row in rows.iterrows():
-        print(row)
+        ticker = rows.at[index, 'Ticker']
+        transaction = rows.at[index, 'Transaction']
+        amount = rows.at[index, 'Amount']
+        date = rows.at[index, 'TransactionDate']
+        if ticker not in shares:
+            return
+
+        
+        
+
+
 
 getreturn("Robert J. Wittman")
